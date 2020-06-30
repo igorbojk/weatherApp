17f9896d49e381eb240faabb3c713321c3b291e7
Igor <igorbojk@gmail.com>
2020-06-30 07:45:12 +0300

    firs commit

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#006a4e',
     alignItems: 'center',
     justifyContent: 'center',
   },
